8937cca98d96e6680e21194ec8030d72c7d88cf2
Steven <steven@ceriously.com>
2016-06-03 17:18:35 -0400

    Add echo message

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const http = require('http');
+const url = require('url');
 
 const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  var obj = {
-      'message': 'Hello world',
-      'env': process.env
-  };
-  res.end(JSON.stringify(obj));
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+
+    var message = (query && query.message) ? query.message : 'Hello World';
+    var obj = {
+        'message': message,
+        'env': process.env
+    };
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(obj));
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
